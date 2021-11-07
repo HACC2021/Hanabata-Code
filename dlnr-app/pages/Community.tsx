f65ce40e206bc1ec9f8e5ac782424bc5e2915b14
yongkim93 <yong4@hawaii.edu>
2021-11-06 14:48:04 -1000

    add drawer

--- dlnr-app/pages/Community.tsx
+++ dlnr-app/pages/Community.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
-import AllLinkButton from "../components/AllLinkButton";
 
 export default function Community({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Community Screen</Text>
-	<AllLinkButton navigation={navigation}/>
     </View>
   );
 }
