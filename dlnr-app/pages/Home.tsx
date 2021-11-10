f82101f5cf1b894f4c5c7171100eb49b615b9edc
keith okuna <keithokuna@gmail.com>
2021-11-10 01:12:27 -0800

    navigation from map working

--- dlnr-app/pages/Home.tsx
+++ dlnr-app/pages/Home.tsx
@@ -1,32 +1,53 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import { Marker, Callout } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { useEffect } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TrailDetail from './TrailDetail';
 
 export default function Home(props) {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Map">
+        {p => <MapComponent {...props} />}
+      </Stack.Screen>
+      <Stack.Screen name="TrailDetail" component={TrailDetail} />
+    </Stack.Navigator>
+  );
+}
 
+const MapComponent = (props) => {
   return (
     <View style={styles.container}>
       <MapView initialRegion={{
-      latitude: 21.4389,
-      longitude: -158,
-      latitudeDelta: 0.5,
-      longitudeDelta: 0.5, }}
-      style={styles.map}>
+        latitude: 21.4389,
+        longitude: -158,
+        latitudeDelta: 0.5,
+        longitudeDelta: 0.5,
+      }}
+        style={styles.map}>
 
-      { props.trails.map((trail, index) => (
-        <Marker
-          key={index}
-          coordinate={trail.coords}
-          title={trail.name}
-          description={trail.description}
-          />
+        {props.trails.map((trail, index) => (
+          <Marker
+            key={index}
+            coordinate={trail.coords}
+            title={trail.name}
+            description={trail.description}
+            onCalloutPress={() => props.navigation.navigate('TrailDetail', { trail })}
+          >
+          <Callout>
+            <View>
+              <Text>{trail.name}</Text>
+            </View>
+          </Callout>
+          </Marker>
         ))}
-
       </MapView>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
