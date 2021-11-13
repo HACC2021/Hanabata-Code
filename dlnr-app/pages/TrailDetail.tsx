f8669e469202eb273f81f2fc65cbeda440ad3ed3
Yeji Han <hany7@hawaii.edu>
2021-11-13 00:46:07 -1000

    Update TrailDetail.tsx

--- dlnr-app/pages/TrailDetail.tsx
+++ dlnr-app/pages/TrailDetail.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, View, Text, Image } from 'react-native';
 import { useEffect } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 export default function TrailDetail(props) {
     let trail = props.route.params.trail;
     return (
-        <ScrollView>
-            <Text>{trail.name}</Text>
-        </ScrollView>
+        <>
+            <Card>
+                <Card.Title>{trail.name}</Card.Title>
+                <Card.Divider/>
+                <Card.Image source={{uri: trail.image}}></Card.Image>
+                <Card.Divider/>
+                <Text style={{marginBottom: 10}}>
+                    {trail.description}
+                </Text>
+                <Card.Divider/>
+                <Text></Text>
+            </Card>
+        </>
     );
 }
 
