64fc06fe24c16b695d9778e1bd6ce35f93d6e540
Yeji Han <hany7@hawaii.edu>
2021-11-12 14:52:42 -1000

    community component (unused)

--- dlnr-app/pages/Community.tsx
+++ dlnr-app/pages/Community.tsx
@@ -1,10 +1,33 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {  View } from "react-native";
+import {Avatar, ListItem} from 'react-native-elements';
 
 export default function Community({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Community Screen</Text>
-    </View>
+    const list = [
+        {
+            name: 'Amy Farha',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+            subtitle: 'Vice President'
+        },
+        {
+            name: 'Chris Jackson',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+            subtitle: 'Vice Chairman'
+        },
+        ]
+    return (
+        <View>
+            {
+                list.map((l, i) => (
+                    <ListItem key={i} bottomDivider>
+                        <Avatar source={{uri: l.avatar_url}} />
+                        <ListItem.Content>
+                            <ListItem.Title>{l.name}</ListItem.Title>
+                            <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))
+            }
+        </View>
   );
 }
