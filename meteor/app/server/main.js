1abe84a5d453f6c524ac7bf0b176de76e4bd204b
yongkim93 <yong4@hawaii.edu>
2021-11-12 15:53:42 -1000

    forgot importing api

--- meteor/app/server/main.js
+++ meteor/app/server/main.js
@@ -3,6 +3,7 @@ import '/imports/startup/server/Publications';
 import '/imports/startup/server/Mongo';
 import '/imports/startup/server/Methods';
 import '/imports/startup/server/Login';
+import '/imports/startup/server/DlnrApp';
 
 import { Trails } from '../imports/api/trail/Trail';
 
